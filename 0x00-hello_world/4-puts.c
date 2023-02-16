d0fcbd5a4e77343588e6038e9c56d0a9d87d3645
chefslemz <slemoslenda@gmail.com>
2023-02-16 15:55:12 -0800

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"programming i like building a multilingual pussle");
+	return (0);
+}
